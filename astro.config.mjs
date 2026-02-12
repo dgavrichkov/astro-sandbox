72e8acd1ca15d8d8c554d28fad3ef841415f542b
dgavrichkov <dgavrichkiv32@gmail.con>
2026-02-12 14:51:10 +0400

    install tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,13 @@ import { defineConfig } from "astro/config";
 import vue from "@astrojs/vue";
 import preact from "@astrojs/preact";
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [preact(), vue()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
